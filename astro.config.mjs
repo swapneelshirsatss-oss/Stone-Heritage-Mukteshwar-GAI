5f1d649f9f5ffe3a82dc8df2b966d42a3f194e3e
swapneelshirsatss-oss <swapneel.shirsat.ss@gmail.com>
2026-07-18 23:56:48 +0530

    Revert "Automate sitemap generation using @astrojs/sitemap"
    This reverts commit 969b10d8587400bdf30597f44900dafe142f1ab9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thestoneheritage.in',
-  integrations: [react(), sitemap()],
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
     ssr: {
