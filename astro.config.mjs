969b10d8587400bdf30597f44900dafe142f1ab9
swapneelshirsatss-oss <swapneel.shirsat.ss@gmail.com>
2026-07-18 23:52:46 +0530

    Automate sitemap generation using @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://thestoneheritage.in',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     ssr: {
